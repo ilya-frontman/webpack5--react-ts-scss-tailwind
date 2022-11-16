f09a8b5a0018310e202b23e2dfbaaf096b9188f3
ilya.frontman <ilya.frontman@yandex.ru>
2022-11-16 21:13:08 +0300

    update svgo config

--- svgo.config.js
+++ svgo.config.js
@@ -1,23 +1,38 @@
 module.exports = {
   multipass: true, // boolean. false by default
-  datauri: 'enc', // 'base64' (default), 'enc' or 'unenc'.
-  js2svg: {
-    indent: 2, // string with spaces or number of spaces. 4 by default
-    pretty: true, // boolean, false by default
-  },
   plugins: [
-    // set of built-in plugins enabled by default
-    'preset-default',
-
-    // enable built-in plugins by name
-    'prefixIds',
-
-    // or by expanded notation which allows to configure plugin
+    'cleanupEnableBackground',
+    'cleanupAttrs',
+    'removeDoctype',
+    'removeXMLProcInst',
+    'removeComments',
+    'removeMetadata',
+    'removeTitle',
+    'removeDesc',
+    'removeUselessDefs',
+    'removeEditorsNSData',
+    'removeEmptyAttrs',
+    'removeHiddenElems',
+    'removeEmptyText',
+    'removeEmptyContainers',
+    'convertStyleToAttrs',
+    'convertColors',
+    'convertPathData',
+    'convertTransform',
+    'removeUnknownsAndDefaults',
+    'removeNonInheritableGroupAttrs',
+    'removeUselessStrokeAndFill',
+    'removeUnusedNS',
+    'cleanupNumericValues',
+    'moveElemsAttrsToGroup',
+    'moveGroupAttrsToElems',
+    'collapseGroups',
+    'mergePaths',
+    'convertShapeToPath',
+    'sortAttrs',
+    'removeDimensions',
     {
-      name: 'sortAttrs',
-      params: {
-        xmlnsOrder: 'alphabetical',
-      },
+      name: 'preset-default',
     },
   ],
 };
