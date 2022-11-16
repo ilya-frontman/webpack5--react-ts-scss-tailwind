708a34600ae23ff6f39e252466e8614493437e37
ilya.frontman <ilya.frontman@yandex.ru>
2022-11-16 21:25:34 +0300

    test svg sprites: app and styles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Home from 'Svg/home-fill.svg';
+import Mail from 'Svg/mail-fill.svg';
 
 const App: React.FC = () => {
   const message = 'Hello React on webpack rails';
-  return <h1>{message}</h1>;
+  return (
+    <>
+      <h1>{message}</h1>
+      <svg className="icon">
+        <use xlinkHref={Home} />
+      </svg>
+      <svg className="icon">
+        <use xlinkHref={Mail} />
+      </svg>
+    </>
+  );
 };
 
 export default App;
