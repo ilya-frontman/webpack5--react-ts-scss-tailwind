61ed0d208336617ee3c07c856b5e07f6e5536b89
ilya.frontman <ilya.frontman@yandex.ru>
2022-11-17 03:32:32 +0300

    update source tsx and scss

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Home from 'Svg/home-fill.svg';
 import Mail from 'Svg/mail-fill.svg';
 
-const App: React.FC = () => {
+const App = () => {
   const message = 'Hello React on webpack rails';
+
+  // console.log('test');
+
   return (
     <>
       <h1>{message}</h1>
